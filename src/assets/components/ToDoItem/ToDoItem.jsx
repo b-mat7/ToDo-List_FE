51e08f253ba52d3d50e471d423a74284a4be1cd9
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-10-03 19:57:01 +0200

    deleteItem added

--- src/assets/components/ToDoItem/ToDoItem.jsx
+++ src/assets/components/ToDoItem/ToDoItem.jsx
@@ -1,23 +1,47 @@
+import { useContext } from 'react'
+
+import { RefreshContext } from '../../../App'
+
 import styles from './ToDoItem.module.scss'
 
 const ToDoItem = ({ item }) => {
+
+  const { refresh, setRefresh } = useContext(RefreshContext)
+
+  const deleteItem = async () => {
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_LINK}/todos`, {
+        method: "DELETE",
+        headers: {
+          // authorization
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ _id: item._id })
+      })
+
+      if (response.ok) {
+        setRefresh(prev => !prev)
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <article className={styles.todo_item}>
       <div>
         <p>{item.name}</p>
         <p>{item.menge}</p>
-      </div>
-      <div>
         <p>{item.typ}</p>
         <p>{item.ort}</p>
-      </div>
-      <div>
         <p>{item.info}</p>
+        <button onClick={deleteItem}>X</button>
       </div>
 
       {/* edit button */}
       {/* item.status= inaktive -> zeige delete button */}
-      
+
     </article>
   );
 }
