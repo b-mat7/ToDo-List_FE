15ba955ed3b59f2ca0bf542b55c25dad79420189
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-10-04 14:28:24 +0200

    editItem added

--- src/assets/components/ToDoItem/ToDoItem.jsx
+++ src/assets/components/ToDoItem/ToDoItem.jsx
@@ -1,10 +1,50 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { RefreshContext } from '../../../App'
 import styles from './ToDoItem.module.scss'
 
 const ToDoItem = ({ item }) => {
 
+  const [editMode, setEditMode] = useState(false)
+  const [editedItem, setEditedItem] = useState({ ...item })
+
   const { refresh, setRefresh } = useContext(RefreshContext)
+
+
+  const toggleEditMode = () => setEditMode(prev => !prev)
+
+  // Update the edited item's fields while typing
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedItem((prevItem) => ({
+      ...prevItem,
+      [name]: value,
+    }));
+  };
+
+  const editItem = async (event) => {
+    event.preventDefault()
+
+    const form = new FormData(event.target)
+    form.append("bearbeitet", new Date().toISOString())
+    form.append("_id", item._id)
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_LINK}/todos`, {
+        method: "PUT",
+        // headers: {
+        //   authorization
+        // },
+        body: form
+      })
+
+      if (response.ok) {
+        toggleEditMode()
+        setRefresh(prev => !prev)
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
   const deleteItem = async () => {
     try {
@@ -27,24 +67,58 @@ const ToDoItem = ({ item }) => {
 
   return (
     <article className={styles.todo_item}>
-      <div>
-        <p>{item.name}</p>
-        <p>{item.menge}</p>
 
-        <p>{item.typ}</p>
-        <p>{item.ort}</p>
+      {editMode ?
 
-        <p>{item.wer}</p>
-        <p>{item.faellig}</p>
+        <form onSubmit={editItem}>
+          <button type="submit">Save</button>
+          <input type="text" name="name" id="name" value={editedItem.name} onChange={handleInputChange} required />
+          <input type="text" name="menge" id="menge" placeholder="Menge" value={editedItem.menge} onChange={handleInputChange} />
+          <input type="text" name="info" id="info" placeholder="Info" value={editedItem.info} onChange={handleInputChange} />
+          <select name="typ" id="typ" value={editedItem.typ} onChange={handleInputChange}>
+            <option value="einkauf">Einkauf</option>
+            <option value="todo">ToDo</option>
+          </select>
+          <select name="ort" id="ort" value={editedItem.ort} onChange={handleInputChange}>
+            <option value="supermarkt">Supermarkt</option>
+            <option value="apotheke">Apotheke</option>
+            <option value="drogerie">Drogerie</option>
+            <option value="seeInfo">Siehe Info</option>
+          </select>
+          <select name="wer" id="wer" value={editedItem.wer} onChange={handleInputChange}>
+            <option value=""></option>
+            <option value="kersi">Kersi</option>
+            <option value="matze">Matze</option>
+          </select>
+          <input type="date" id="faellig" name="faellig" value={editedItem.faellig} onChange={handleInputChange} />
+          <select name="status" id="status" value={editedItem.status} onChange={handleInputChange}>
+            <option value="aktiv">Aktiv</option>
+            <option value="inaktiv">Inaktiv</option>
+          </select>
+        </form>
 
-        <p>{item.info}</p>
+        :
 
-        <button onClick={deleteItem}>X</button>
-      </div>
+        <div>
+          {item.status === "aktiv" &&
+            <button onClick={toggleEditMode}>Edit</button>
+          }
+          <p>{item.name}</p>
+          <p>{item.menge}</p>
 
-      {/* edit button */}
-      {/* item.status= inaktive -> zeige delete button */}
+          <p>{item.typ}</p>
+          <p>{item.ort}</p>
 
+          <p>{item.wer}</p>
+          <p>{item.faellig}</p>
+
+          <p>{item.info}</p>
+
+          {item.status === "inaktiv" &&
+            <button onClick={deleteItem}>X</button>
+          }
+        </div>
+      }
     </article>
   );
 }
