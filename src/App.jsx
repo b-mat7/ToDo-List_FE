635c8f13394a58cc72573c24f1fd18b0a8f9c5d2
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-10-13 01:14:58 +0200

    login fetch added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <main>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path='/home' element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </main>
   )
--- src/assets/components/pages/Login/Login.jsx
+++ src/assets/components/pages/Login/Login.jsx
@@ -1,9 +1,41 @@
+import { useRef } from 'react';
+
 import styles from './Login.module.scss'
 
 const Login = () => {
+
+  const passRef = useRef()
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_LINK}/login`, {
+        method: "POST",
+        headers: {
+          // siehe http only...
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({passphrase : passRef.current.value})
+      })
+
+      if(response.ok) {
+        const result = await response.json()
+        console.log(result)
+        // navigate /home
+      }
+
+      else {
+        const result = await response.json()
+        console.log(result)
+      }
+
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
   return (
     <section className={styles.login}>
-      <h1>Loginpage</h1>
+      <input ref={passRef} type="text" name="passphrase" id="passphrase" placeholder='Passphrase'/>
+      <button onClick={handleLogin}>Login</button>
     </section>
   );
 }
