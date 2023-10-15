49ff89f836d2eed62783f4ac8f159b3232609860
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-10-15 22:39:59 +0200

    sortBy feature basic finished

--- src/assets/components/shared/ToDosContainer/TodosContainer.jsx
+++ src/assets/components/shared/ToDosContainer/TodosContainer.jsx
@@ -1,26 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import ToDoItem from '../ToDoItem/ToDoItem';
 import styles from './ToDosContainer.module.scss'
 
 const ToDosContainer = ({ filteredToDos }) => {
 
-  const [sortBy, setSortBy] = useState(null)
+  const [sortedToDos, setSortedToDos] = useState([])
+  const [sortBy, setSortBy] = useState("editedAsc")
 
-  const sortedToDos = [...filteredToDos].sort(dueAsc)
+  const nameAsc = (a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+  const nameDes = (a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1
+  const dueAsc = (a, b) => new Date(a.due) - new Date(b.due)
+  const dueDes = (a, b) => new Date(b.due) - new Date(a.due)
+  const editedAsc = (a, b) =>  new Date(a.edited) - new Date(b.edited)
+  const editedDes = (a, b) => new Date(b.edited) - new Date(a.edited)
+  const sortFunctions = { nameAsc, nameDes, dueAsc, dueDes, editedAsc, editedDes }
+
+  useEffect(() => {
+    const sorted = [...filteredToDos].sort(sortFunctions[sortBy])
+    setSortedToDos(sorted)
+  }, [filteredToDos, sortBy])
 
   const sortedActiveToDos = sortedToDos.filter(item => item.active === true)
   const sortedInactiveToDos = sortedToDos.filter(item => item.active === false)
 
-  function nameAsc(a, b) { return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1 }
-  function nameDes(a, b) { return a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1 }
-  function dueAsc(a, b) { return new Date(a.due) - new Date(b.due) }
-  function dueDes(a, b) { return new Date(b.due) - new Date(a.due) }
-  function editedAsc(a, b) { return new Date(a.edited) - new Date(b.edited) }
-  function editedDes(a, b) { return new Date(b.edited) - new Date(a.edited) }
-
   return (
     <section className={styles.toDos_container}>
+      <div>
+        <button onClick={() => setSortBy("nameAsc")}>Name</button>
+        <button onClick={() => setSortBy("editedAsc")}>Edit</button>
+        <button onClick={() => setSortBy("dueAsc")}>Due</button>
+      </div>
       <div className={styles.active}>
         {sortedActiveToDos.map((item) => (
           <ToDoItem
