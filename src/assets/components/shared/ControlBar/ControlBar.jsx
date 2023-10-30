6267e5bf58e0706616e9c82f85ce8345f1901a9b
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-10-30 21:14:02 +0100

    logout button added

--- src/assets/components/shared/ControlBar/ControlBar.jsx
+++ src/assets/components/shared/ControlBar/ControlBar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ToDosContainer from '../ToDosContainer/TodosContainer';
 import styles from './ControlBar.module.scss'
 
@@ -17,6 +18,7 @@ const ControlBar = ({ toDos }) => {
   // const [filteredToDos, setFilteredToDos] = useState([])
   // const [isFiltered, setIsFiltered] = useState(false)
 
+  const navigate = useNavigate()
 
 
   // search functions
@@ -94,6 +96,19 @@ const ControlBar = ({ toDos }) => {
   //   }
   // }
 
+  const handleLogout = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_LINK}/logout`, {
+        credentials: "include"
+      })
+
+      if (response.ok) navigate("/login")
+
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <>
       {isSearchActive
@@ -101,9 +116,12 @@ const ControlBar = ({ toDos }) => {
         : <ToDosContainer filteredToDos={postFilterBtnTodos} />
       }
 
-      <section className={styles.controlbar}>
-        <div className={styles.controlPanel}>
+      <section className={styles.control_bar}>
+        <div className={styles.control_panel}>
           <div className={styles.search_container}>
+            <button onClick={handleLogout} className={styles.logout_btn}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="12px" height="12px" fill={"hsla(0, 0%, 100%, 1)"}><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" /></svg>
+            </button>
             <input
               onChange={handleSearch}
               type="text"
